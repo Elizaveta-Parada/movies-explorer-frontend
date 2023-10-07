02cd1b50dae89145b6686e31b80faa382166e8c5
Elizaveta Parada <parada_ev@mail.ru>
2023-10-07 14:57:34 +0200

    PP route/main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,13 +7,16 @@ import { Link } from 'react-router-dom';
 function Header() {
     return (
         <header className="header">
-            <img className="header__logo" src={Logo} alt="Логотип" />
-            <div className="header__container">
-                <Link to="/signup" className="header__link">Регистрация</Link>
-                <button className="header__btn" type="submit">
-                    <Link className="header__btn-link" to="/signin" >Войти</Link>
-                </button>
+            <div className="header__contant">
+                <img className="header__logo" src={Logo} alt="Логотип" />
+                <div className="header__container">
+                    <Link to="/signup" className="header__link">Регистрация</Link>
+                    <button className="header__btn" type="submit">
+                        <Link className="header__btn-link" to="/signin" >Войти</Link>
+                    </button>
+                </div>
             </div>
+
         </header>
     );
 }
